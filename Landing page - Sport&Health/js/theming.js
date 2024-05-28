a42d37851d16d8eaede2c72701262e556429aa97
Leandro <leandropifano@gmail.com>
2024-05-28 20:49:36 -0300

    ajusto teming en sporthealth
    cambio a azules por defecto y agrego naranjas como cuarto tema para dar opciones mas distintas entre ellas.

--- Landing page - Sport&Health/js/theming.js
+++ Landing page - Sport&Health/js/theming.js
@@ -11,6 +11,11 @@ function seteo_selects(){
 function seteo_color(){
   let color_elegido = document.getElementById('theme-color').value;
   switch (color_elegido){
+    case "color-00":
+      document.documentElement.style.setProperty('--first-color', '#333c87');
+      document.documentElement.style.setProperty('--second-color', '#000080');
+      document.documentElement.style.setProperty('--third-color', '#5dc1b9');
+    break;
     case "color-01":
       document.documentElement.style.setProperty('--first-color', '#a5a5a5');
       document.documentElement.style.setProperty('--second-color', '#665d5d');
@@ -22,9 +27,9 @@ function seteo_color(){
       document.documentElement.style.setProperty('--third-color', '#d597ae');
     break;
     case "color-03":
-      document.documentElement.style.setProperty('--first-color', '#b0041c');
-      document.documentElement.style.setProperty('--second-color', '#d52d4e');
-      document.documentElement.style.setProperty('--third-color', '#a5a5a5');
+      document.documentElement.style.setProperty('--first-color', '#ffa420');
+      document.documentElement.style.setProperty('--second-color', '#e25f23');
+      document.documentElement.style.setProperty('--third-color', '#cc7722');
     break;
   }
 }
